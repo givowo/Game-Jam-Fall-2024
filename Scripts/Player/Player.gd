e67858153b7440165ab8fb4b139d29d4085822bf
Chopppers <53385144+Chopppers@users.noreply.github.com>
2024-10-18 20:00:50 -0500

    character based interactions + shader for color swaping

--- Scripts/Player/Player.gd
+++ Scripts/Player/Player.gd
@@ -2,14 +2,26 @@ extends CharacterBody2D
 
 var speed = 100 # speed in delta units (not pixels. argh...)
 var _char = GlobalPref.main_char
+var can_interact = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("debug_character"):
+			GlobalPref.main_char = fmod((GlobalPref.main_char + 1), GlobalPref.charname.size())
+			update_character()
+	
+	
 	var input_dir = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
 	velocity = input_dir * speed
 	move_and_slide()
+	
+	can_interact = false #disables after everything so when objects set it doesnt instantly disapear lol
+
+func update_character():
+	_char = GlobalPref.main_char
--- Scripts/candle.gd
+++ Scripts/candle.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
 var interacted = false
+var is_interactable = false
 
+var color = [0,0.5,0.7]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,7 +12,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$CollisionShape2D.debug_color = Color(0,0,1,0.5)
-	 
+	is_interactable = false
 	if interacted:
 		$CollisionShape2D.debug_color = Color(1,0,1,0.5)
 	
@@ -18,8 +20,13 @@ func _process(delta: float) -> void:
 	
 	for i in bodys.size():
 		if bodys[i] is CharacterBody2D:
+			if !interacted:
+				is_interactable = true
+				$Interact.material.set_shader_parameter("cycleOffset",color[bodys[i]._char])
 			if Input.is_action_just_pressed("interact_object") && !interacted:
 				interacted = true
+				
+	$Interact.visible = is_interactable
 
 func _on_body_entered(body: Node2D) -> void:
 	pass
